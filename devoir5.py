bdd3aa8907fa4c7d90ffd5cb93d0355659141391
yrxmr <59065688+yrxmr@users.noreply.github.com>
2020-04-30 13:10:01 +0200

    Update and rename devoir5 to devoir5.py

--- devoir5.py
+++ devoir5.py
@@ -3,6 +3,7 @@ import spacy
 with open("/home/y/Documents/sequoia.txt","r") as f, open("/home/y/Documents/orfeo.txt","r") as f2, open("/home/y/Documents/bashung.txt") as f3:
     f = f.read()
     f2 = f2.read()
+    f3 = f3.read()
     nlp = spacy.load("fr_core_news_md")
     doc = nlp(f)
     doc2 = nlp(f2)
